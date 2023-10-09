5dfcad8e4bbe94e3c21d236588a60cade9dfcf46
Shivanand <shivanandnaduvin@gmail.com>
2023-10-09 12:08:45 +0530

    Update start_tunnel.sh

--- tunneling_daemon_service/start_tunnel.sh
+++ tunneling_daemon_service/start_tunnel.sh
@@ -1,1 +1,4 @@
 /home/sigmoid/code tunnel ##u need manualy run this and logged in first
+
+#make sure file `code` has execute permission, if not execute below command once
+# chmod +x /home/sigmoid/code
